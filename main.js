1521db208e64b9120aad81cf947787cd6d1b68e1
Jonathan Altamirano <jona-mo-7@hotmail.com>
2025-03-30 23:54:53 -0300

    Tarea 3 js

---
+++ main.js
@@ -0,0 +1,80 @@
+
+function agregarTarea() {
+    var form = document.getElementById("entrada");
+    var lista_tareas = document.getElementById("tasklist");
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        var task = document.getElementById("tarea");
+        if (task.value.length >= 1) {
+            var nueva_tarea = document.createElement('div');
+            nueva_tarea.classList = 'task';
+            nueva_tarea.innerHTML = `<li> <button>check</button> <p>${task.value}</p> </li>  <button onclick=eliminarTarea() class = "dlt-btn"> <img src="tacho.png" height="15px" class="ej"></button>`;
+            lista_tareas.append(nueva_tarea);
+            alert('Tarea agregada');
+            task.value = '';
+        } else {
+            alert("Por favor ingrese una tarea válida");
+        }
+    })
+}
+
+function completarTarea() {
+    /*document.addEventListener("click", function (e) {
+        if (e.target.classList == 'cmplt-btn') {
+            var tarea = e.target.closest(".task");
+            var tareai = e.target.closest(".completada");
+            if(tarea){
+                tarea.classList = 'completada';
+            } else if(tareai)
+            {
+                tareai.classList = 'task';
+            }
+            
+        }
+    })*/
+    document.addEventListener("click", function (e) {
+        if (e.target.classList == 'cmplt-btn') {
+            var tarea = e.target.closest(".task");
+        }
+        tarea.classList.toggle("completada");
+    })
+}
+
+function eliminarTarea() {
+            document.addEventListener("click", function (e) {
+                if (e.target.classList == 'dlt-btn') {
+                    var tarea = e.target.closest(".task");
+                    if (tarea) { tarea.remove(); }
+                }
+            })
+
+        }
+
+function eliminarCompletadas() {
+            var tareas = document.querySelectorAll('.completada')
+            console.log(tareas);
+            tareas.forEach(x => x.remove());
+        }
+
+function mostrarCompletadas() {
+            var tareas = document.querySelectorAll('.task')
+            tareas.forEach(x => x.classList = 'oculto')
+            var tareasc = document.querySelectorAll('.ocultoc')
+            tareasc.forEach(x => x.classList = 'completada')
+        }
+
+function mostrarFaltantes() {
+            var tareasc = document.querySelectorAll('.completada')
+            tareasc.forEach(x => x.classList = 'ocultoc')
+            var tareas = document.querySelectorAll('.oculto')
+            tareas.forEach(x => x.classList = 'task')
+        }
+
+function mostrarTodas() {
+            var tareasc = document.querySelectorAll('.ocultoc')
+            tareasc.forEach(x => x.classList = 'completada')
+            var tareas = document.querySelectorAll('.oculto')
+            tareas.forEach(x => x.classList = 'task')
+
+        }
